9d38666565cf16320e72527ccefe981c077e8bb2
uhleesuuuh <espiritu.alisa@gmail.com>
2023-06-26 11:19:06 +0800

    changed the position of middlewares

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ import models from './src/models';
 import routes from './src/routes';
 
 const app = express();
-app.use(cors());
-
-app.use('/users', routes.user);
-app.use('/messages', routes.message);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     req.context = {
@@ -70,3 +63,11 @@ app.use((req, res, next) => {
     req.serverMessage = 'server generated message';
     next();
 });
+
+app.use(cors());
+
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
